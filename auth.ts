5ba899a1a0d1b0054e4256ab141c8af86ee3f5ea
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-15 03:15:30 -0700

    Configure Auth

---
+++ auth.ts
@@ -0,0 +1,41 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import {User} from '@/lib/definitions/user'
+
+import { Admin } from './lib/constants/admin';
+
+function getPassword(): string {
+  const password = process.env.PASSWORD;
+  if (!password) {
+    throw new Error("PASSWORD environment variable is not set");
+  }
+  return password;
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials): Promise<User | null> {
+        const parsedCredentials = z
+          .object({ password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { password } = parsedCredentials.data;
+          const adminPassword = getPassword();
+          if (!adminPassword) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, adminPassword);
+          if (passwordsMatch) return Admin;
+        }
+
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
