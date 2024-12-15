05e8b6c70b363c10271140380dca92f2c3bab6b9
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-15 04:22:49 -0700

    Add Image Upload

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+import ImageUpload from "@/ui/image_upload";
+
+export default function Admin() {
+  return (
+    <main className="">
+      <section className="container">
+        <ImageUpload />
+      </section>
+    </main>
+  );
+}
