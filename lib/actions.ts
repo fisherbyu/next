5a7d958022531d354863da7506f59aa960b4acf8
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-15 17:47:16 -0700

    Redirect to /admin from /login

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,14 +1,22 @@
 'use server';
- 
+
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-  
+import { redirect } from 'next/navigation';
+
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+
+    // If we get here, sign-in was successful
+    redirect('/admin');
+    
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
