bbefba14376cdc6a5e7eb6700b545602ad7ffc87
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-16 21:59:46 -0700

    Put in section

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,10 +2,8 @@ import ImageUpload from '@/ui/image_upload';
 
 export default function Admin() {
 	return (
-		<main>
-			<section className="container">
-				<ImageUpload />
-			</section>
-		</main>
+		<section className="container">
+			<ImageUpload />
+		</section>
 	);
 }
