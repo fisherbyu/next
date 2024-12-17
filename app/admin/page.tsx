2390ef7bbfe4f56d69aeba9e01ecf8e24df3ed5f
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-17 00:51:12 -0700

    Design Admin Layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,3 @@
-import PageTitle from '@/components/ui/page-title';
 import ImageUpload from '@/ui/image-upload';
 import { Button, Divider } from 'thread-ui';
 
@@ -9,18 +8,19 @@ const title: { title: string; subtitle?: string; center?: boolean } = {
 
 export default function Admin() {
 	return (
-		<section className="container">
+		<>
 			<div className="container my-5 w-6/12">
-				<div className="flex flex-row w-full mx-auto justify-between">
-					<h1 className=" text-center  dark:text-white text-3xl md:text-4xl xl:text-5xl font-medium text-gray-900">Admin</h1>
-					<Button>Sign Out</Button>
+				<div className="flex flex-row w-full justify-between">
+					<h1 className="text-center dark:text-white text-3xl md:text-4xl xl:text-5xl font-medium text-gray-900">Admin</h1>
+					<Button margin="0px">Sign Out</Button>
 				</div>
 				<div className="mx-auto py-4 w-full">
 					<Divider width="100%" />
 				</div>
 			</div>
-			<PageTitle components={title} />
-			<ImageUpload />
-		</section>
+			<section className="container">
+				<ImageUpload />
+			</section>
+		</>
 	);
 }
