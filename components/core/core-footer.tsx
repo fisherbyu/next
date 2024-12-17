8d5a88335c138304edd7e1f58fa3d33d2ce20248
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-16 21:38:16 -0700

    Update footer

--- components/core/core-footer.tsx
+++ components/core/core-footer.tsx
@@ -4,22 +4,23 @@ import LinkedInIcon from '@/public/core/linkedin.svg';
 import { Separator } from '@/components/ui/separator';
 import AFLogo from '@/public/core/af-logo.svg';
 import Link from 'next/link';
+import { Divider } from 'thread-ui';
 
 export default function CoreFooter() {
 	return (
-		<footer className="w-full mt-auto">
+		<footer className="h-52 w-full">
 			<div className="container mx-auto">
-				<Separator />
-				<div className="py-12 mx-auto overflow-hidden max-w-7xl">
-					<p className="mt-8 text-center">
+				<Divider width="100%" />
+				<div className="">
+					<p className="mt-4 text-center">
 						<Link href="/">
 							<Image src={AFLogo} alt="AF" className="mx-auto" />
 						</Link>
-						<span className="mx-auto mt-2 text-sm text-gray-500">
+						<span className="mx-auto mt-1 text-sm text-gray-500">
 							<Link href="/login">Created by Andrew Fisher</Link>
 						</span>
 					</p>
-					<div className="flex justify-center mt-8 space-x-6">
+					<div className="flex justify-center my-2 space-x-6">
 						<span className="inline-flex justify-center w-full gap-3 m-auto md:justify-start md:w-auto">
 							<a href="https://github.com/fisherbyu" className="w-6 h-6 transition fill-black hover:text-blue-500">
 								<span className="sr-only">github</span>
