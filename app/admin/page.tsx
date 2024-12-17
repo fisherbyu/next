7c5272638e26485bf2efd5954f5146854882a457
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-17 00:54:32 -0700

    Configure Admin Layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -12,7 +12,9 @@ export default function Admin() {
 			<div className="container my-5 w-6/12">
 				<div className="flex flex-row w-full justify-between">
 					<h1 className="text-center dark:text-white text-3xl md:text-4xl xl:text-5xl font-medium text-gray-900">Admin</h1>
-					<Button margin="0px">Sign Out</Button>
+					<Button color="grey" margin="0px">
+						Sign Out
+					</Button>
 				</div>
 				<div className="mx-auto py-4 w-full">
 					<Divider width="100%" />
