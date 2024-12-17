b557a175f9a17da469f1aab6ee35a41b1b267965
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-17 00:31:51 -0700

    Work on Admin Title

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,6 @@
 import PageTitle from '@/components/ui/page-title';
 import ImageUpload from '@/ui/image-upload';
+import { Button, Divider } from 'thread-ui';
 
 const title: { title: string; subtitle?: string; center?: boolean } = {
 	title: 'Add a Photo',
@@ -9,6 +10,15 @@ const title: { title: string; subtitle?: string; center?: boolean } = {
 export default function Admin() {
 	return (
 		<section className="container">
+			<div className="container my-5 w-6/12">
+				<div className="flex flex-row w-full mx-auto justify-between">
+					<h1 className=" text-center  dark:text-white text-3xl md:text-4xl xl:text-5xl font-medium text-gray-900">Admin</h1>
+					<Button>Sign Out</Button>
+				</div>
+				<div className="mx-auto py-4 w-full">
+					<Divider width="100%" />
+				</div>
+			</div>
 			<PageTitle components={title} />
 			<ImageUpload />
 		</section>
